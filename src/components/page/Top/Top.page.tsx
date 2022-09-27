862a8957f2d24f4e68ead00fe1d2a67b979c35bf
as_sqr <ryo.yossy@live.jp>
2022-09-27 18:45:12 +0900

    Initial commit

---
+++ src/components/page/Top/Top.page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Header } from '@/components/ui/Header'
+import { Content } from '@/components/ui/Content'
+
+import { Top } from './Top'
+
+export const TopPage = () => (
+  <>
+    <Header />
+
+    <Content>
+      <Top />
+    </Content>
+  </>
+)
